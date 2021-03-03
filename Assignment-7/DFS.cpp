c6880bf68b11406a5d17a3f6cde076902975acd5
shivamp1998 <shivampandey98@gmail.com>
2021-03-03 23:46:42 +0530

    Assignment

---
+++ Assignment-7/DFS.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void addEdge(vector<int> adj[],int u,int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void BFSrec(vector<int> adj[],bool visited[],int source){
+    visited[source] = true;
+    cout<<source<<endl;
+    for(int i=0;i<adj[source].size();i++){
+        if(visited[i]==false){
+            BFSrec(adj,visited,i);
+        }
+    }
+}
+void BFS(vector<int> adj[],int V){
+    bool visited[V] = {false};
+    BFSrec(adj,visited,0);
+}
+int main(){
+    int V;cin>>V;
+    vector<int> adj[V];
+    for(int i=0;i<V;i++){
+        int x,y;cin>>x>>y;
+        addEdge(adj,x,y);
+    }
+    BFS(adj,V);
+}
