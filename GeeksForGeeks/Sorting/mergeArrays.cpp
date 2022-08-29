da95b1c1942fc0fb984fb4b7931b8f3a82f97500
shivamp1998 <shivampandey98@gmail.com>
2022-08-29 21:19:41 +0530

    [Merge]

---
+++ GeeksForGeeks/Sorting/mergeArrays.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int a[],int b[], int m,int n) {
+    int i = 0 , j = 0;
+    int temp[m+n-1] = {0};
+    int k = 0;
+    while(i < m && j < n) {
+        if(a[i] > b[j]) {
+            temp[k] = b[j];
+            k++;
+            j++;
+        }else{
+            temp[k] = a[i];
+            k++;
+            i++;
+        }
+    }
+    for(int i=0;i<m+n-1;i++) {
+        cout<<temp[i]<<" ";
+    }
+}
+
+int main() {
+    int arr1[] = {2,5,7,9,11};
+    int arr2[] = {1,3,4,6,10,12};
+    merge(arr1,arr2,5,6);
+}
