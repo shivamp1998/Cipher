b1cc973ab8c82a18d2d00d70cac7ef96bb5e0570
shivamp1998 <shivampandey98@gmail.com>
2022-09-06 18:05:15 +0530

    [Fixes]

---
+++ GeeksForGeeks/Sorting/mergeSort.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge() {
+
+}
+void mergeSort(int arr, int l, int m, int r) {
+
+}
+
+int main()  {
+    int arr[10] = {2,1,5,3,1,4,5,6,4,2};
+}
