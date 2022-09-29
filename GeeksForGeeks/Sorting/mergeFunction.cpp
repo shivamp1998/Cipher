e101bb457e5060fffee9173b0eedabbb65a0d23c
shivamp1998 <shivampandey98@gmail.com>
2022-09-29 23:12:22 +0530

    [Fixes]

--- GeeksForGeeks/Sorting/mergeFunction.cpp
+++ GeeksForGeeks/Sorting/mergeFunction.cpp
@@ -1,31 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
-void sortFunction(int a[],int low,int mid,int high) {
-    int t1[ mid+1];
-    int t2[high-mid];
-    for(int i = 0 ; i <= mid ; i++) {
-        t1[i] = a[i];
+void merge(int a[],int low,int mid,int high) {
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    int left[n1] = {0};
+    int right[n2] = {0};
+    
+    for(int i=0;i<n1;i++) {
+        left[i] = a[low + i];
     }
-    for(int i=0;i<high;i++) {
-        cout<<a[i]<<endl;
+    for(int i=0;i<n2;i++) {
+        right[i] = a[mid + 1 + i];
     }
-    for(int i = mid + 1 ;i<  high ; i++) {
-        t2[i] = a[i];
-    }
-    int j = 0 , k = 0 ;
-    while(j < mid + 1 && k < high - low ) {
-        if(t1[j] > t2[k]) {
-            cout<<t2[k]<<" ";
+
+    int i = 0 , j = 0,k = 0;
+    while(i < n1 && j < n2) {
+        if(left[i] < right[j]) {
+            a[k] = left[i];
+            i++;
             k++;
         }else{
-            cout<<t1[j]<<" ";
+            a[k] = right[j];
             j++;
+            k++;
         }
+    }
+
+    while( i < n1 ) {
+        a[k] = left[i];
+        i++;
+        k++;
+    } 
+    
+    while( j < n2 ) {
+        a[k] = right[j];
+        j++;
+        k++;
+    }
+
+    for(int i=0;i<=high;i++) {
+        cout<<a[i]<<" ";
     }
 
     
 }
+
 int main() {
-    int arr[5] = {10,15,20,11,30};
-    sortFunction(arr,0,2,5);
+    int a[5] = {10,15,20,11,30};
+    merge(a,0,2,4);
 }
