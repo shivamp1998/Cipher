b16c3facb791e92a120c6399a41b9eb827d1941a
shivamp1998 <shivampandey98@gmail.com>
2022-10-09 16:30:54 +0530

    fix

--- GeeksForGeeks/Hashing/zeroSumSubarray.cpp
+++ GeeksForGeeks/Hashing/zeroSumSubarray.cpp
@@ -1,11 +1,43 @@
 #include<bits/stdc++.h>
+#include<unordered_set>
 using namespace std;
 
 bool zeroSumSubarray(int *arr, int N) {
+    vector<int> pre(1000,0);
+    int sum = 0;
     for(int i=0;i<N;i++) {
+        sum += arr[i];
+        pre[sum]++;
     }
+    if(pre[0] == 1) {
+        return true; 
+    }
+    for(int i=0;i<1000;i++) {
+        if(pre[i]>1) {
+            return true;
+        } 
+    }
+    return false;
+
+}
+
+bool zeroSumArraySet(int *arr,int N) {
+    unordered_set<int> s;
+    int current_sum = 0;
+    for(int i=0; i<N; i++) {
+        current_sum += arr[i];
+        if(s.find(current_sum) != s.end()){
+            return true;
+        }
+        if(current_sum == 0)
+            return true;
+        s.insert(current_sum);
+    }
+    return false;
 }
 int main() {
     int arr[] = {1,4,-3,1,2};
-    zeroSumSubarray(arr,5);
+    if(zeroSumSubarray(arr,5)) {
+        cout<<"YES";
+    }else cout<<"NO";
 }
