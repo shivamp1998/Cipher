4890a73c03fbe8750185fd44818a01df5bf34ee4
shivamp1998 <shivampandey98@gmail.com>
2022-10-17 21:30:38 +0530

    overalapping merge

---
+++ GeeksForGeeks/Sorting/mergeOverlappingIntervals.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct interval {
+    int start;
+    int end;
+};
+
+void mergeInterval(int arr[], int n) {
+    
+}
+
+int main() {
+    vector<interval> a;
+    for(int i=0;i<4;i++) {
+        interval o;
+        cin>>o.start>>o.end;
+        a.push_back(o);
+    }
+    
+}
