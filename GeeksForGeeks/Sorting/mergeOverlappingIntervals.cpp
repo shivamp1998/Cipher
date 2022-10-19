b55b5ff832c93f6d437fb6d7fb2a24f28f67f654
shivamp1998 <shivampandey98@gmail.com>
2022-10-19 21:38:43 +0530

    merging arrays

--- GeeksForGeeks/Sorting/mergeOverlappingIntervals.cpp
+++ GeeksForGeeks/Sorting/mergeOverlappingIntervals.cpp
@@ -4,9 +4,21 @@ struct interval {
     int start;
     int end;
 };
+bool cmp( interval a, interval b) {
+    return a.start < b.start;
+}
 
-void mergeInterval(int arr[], int n) {
-    
+int mergeInterval(vector<interval> &a, int n) {
+    int res = 0;
+    for(int i=1;i<n;i++) {
+        if(a[res].end >= a[i].start) {
+            a[res].start = min(a[res].start, a[i].start);
+            a[res].end = max(a[res].end,a[i].end);
+        }else{
+            res++;
+            a[res] = a[i];
+        }
+    }
 }
 
 int main() {
@@ -16,8 +28,10 @@ int main() {
         cin>>o.start>>o.end;
         a.push_back(o);
     }
-    for(int i=0;i<4;i++) {
-        cout<<a[i].start<<" "<<a[i].end<<endl;
+    sort(a.begin(), a.end(), cmp);
+    int ans = mergeInterval(a,4);
+    for(int i=0;i<ans;i++) {
+        cout<<a[i].start<<" "<<a[i].end<<" ";
+        cout<<endl;
     }
-    
 }
